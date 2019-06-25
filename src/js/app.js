3357fb1c797aceba8afb0b8de6abc3d775806879
Anatoliy Vasilev <izze.pochta@gmail.com>
2019-06-25 13:59:19 +0300

    animation

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ import {main} from './moduels'
 import Vue from 'vue'
 import Clocks from './components/Clocks.vue'
 import BackgroundSlider from './components/BackgroundSlider.vue'
+import TitleAnimation from './components/TitleAnimation.vue'
 
 window.onload = () => {
     main()
@@ -11,7 +12,15 @@ window.onload = () => {
 
 Vue.component('clocks', Clocks);
 Vue.component('bg-slider', BackgroundSlider);
+Vue.component('t-animation', TitleAnimation);
 
 const app = new Vue({
     el: '#app'
 });
+
+// ----------------------Instances-of-Vue------------------------------------>
+
+
+
+
+
---
+++ src/js/components/TitleAnimation.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="content-title__wrapper">
+            <transition name="slide-fade">
+                <div class="content__title" v-if="show">Welcome</div>
+            </transition>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TitleAnimation",
+        data() {
+            return {
+                show: false
+            }
+        },
+        mounted() {
+            this.show = true;
+        }
+    }
+</script>
+
+<style scoped>
+    .slide-fade-enter-active {
+        transition: all 2.5s ease-out;
+    }
+    .slide-fade-leave-active {
+        transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+    .slide-fade-enter, .slide-fade-leave-to {
+        transform: translateX(400px);
+        opacity: 0;
+    }
+</style>
