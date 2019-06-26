ca8f5aeafda0d178a5061326029a8d5b98b3e1a8
Anatoliy Vasilev <izze.pochta@gmail.com>
2019-06-26 16:47:22 +0300

    clocks

--- src/js/components/Clocks.vue
+++ src/js/components/Clocks.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-        <div>{{hours}}:{{minutes}}:{{seconds}}</div>
+        <div class="clocks-wrap">
+            <transition name="slide-fade">
+                <div class="clocks-hours" :key="hours">{{hours}}:</div>
+            </transition>
+            <transition name="slide-fade">
+                <div class="clocks-minutes" :key="minutes">{{minutes}}:</div>
+            </transition>
+            <transition name="slide-fade">
+                <div class="clocks-seconds" :key="seconds">{{seconds}}</div>
+            </transition>
+        </div>
     </div>
 </template>
 
@@ -11,14 +21,15 @@
             return {
                 hours: 0,
                 minutes: 0,
-                seconds: 0
+                seconds: 0,
+                show: false
             }
         },
         mounted() {
 
             let date = new Date()
             this.hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
-            this.minutes =  date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
+            this.minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
             this.seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
 
             setInterval(() => {
@@ -30,9 +41,20 @@
             timer() {
                 let date = new Date()
                 this.hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
-                this.minutes =  date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
+                this.minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
                 this.seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
             }
         }
     }
 </script>
+
+<style scoped>
+    .slide-fade-enter-active {
+        transition: all 1.1s ease-out;
+    }
+
+    .slide-fade-enter {
+        transform: translateX(70px);
+        opacity: 0;
+    }
+</style>
